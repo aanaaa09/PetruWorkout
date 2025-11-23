21d0a48c66574675f62392d3ae872043624319a3
Ana Seseña <aesesrm@gmail.com>
2025-11-23 12:09:48 +0100

    arcchivos para seo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          VITE_SITE_NAME: process.env.VITE_SITE_NAME,
+          VITE_SITE_DESCRIPTION: process.env.VITE_SITE_DESCRIPTION,
+          VITE_SITE_URL: process.env.VITE_SITE_URL,
+          VITE_CONTACT_EMAIL: process.env.VITE_CONTACT_EMAIL,
+          VITE_CONTACT_PHONE: process.env.VITE_CONTACT_PHONE
+        }
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
