a23ceca9617abbbe71dadfdca2cd30f801c7a725
Ana Seseña <aesesrm@gmail.com>
2025-11-23 12:31:32 +0100

    modificar vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    createHtmlPlugin({
-      inject: {
-        data: {
-          VITE_SITE_NAME: process.env.VITE_SITE_NAME,
-          VITE_SITE_DESCRIPTION: process.env.VITE_SITE_DESCRIPTION,
-          VITE_SITE_URL: process.env.VITE_SITE_URL,
-          VITE_CONTACT_EMAIL: process.env.VITE_CONTACT_EMAIL,
-          VITE_CONTACT_PHONE: process.env.VITE_CONTACT_PHONE
-        }
-      }
-    })
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
