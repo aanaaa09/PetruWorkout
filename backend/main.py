818c5c9b9582ac32097cbedc9a324dd4bea14641
Ana Seseña <aesesrm@gmail.com>
2025-12-02 18:01:11 +0100

    modificar cors

--- backend/main.py
+++ backend/main.py
@@ -43,6 +43,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:5000",
+        "https://petrucalistenia.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
