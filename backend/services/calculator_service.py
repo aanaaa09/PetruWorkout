dfd71340222f9ee26b2304348f179d4bc52a1989
Ana Seseña <aesesrm@gmail.com>
2026-04-12 17:19:06 +0200

    cambiar formula a mflin correcta

--- backend/services/calculator_service.py
+++ backend/services/calculator_service.py
@@ -39,12 +39,11 @@ def calculate_nutrition(
     height_m = height / 100
     bmi = round(weight / (height_m ** 2), 1)
 
-    # BMR
+    #GEB
     if gender == "male":
-        bmr = round(66.5 + (13.397 * weight) + (4.799 * height) - (5.677 * age))
+        bmr = round((10 * weight) + (6.25 * height) - (5 * age) + 5)
     else:
-        bmr = round(655.1 + (9.247 * weight) + (3.098 * height) - (4.330 * age))
-
+        bmr = round((10 * weight) + (6.25 * height) - (5 * age) - 161)
     # TDEE
     tdee = round(bmr * ACTIVITY_MULTIPLIERS[activity_level])
 
